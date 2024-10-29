7cdf54516cdb5336062d4ae18bc8f9f4c43dbd71
Naveen <naveendevgeek@gmail.com>
2024-10-29 01:44:27 -0700

    chore: added the logic of display none and family basic setup

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '@nextui-org/react';
+
+const SampleButton = () => {
+  return <Button color='primary'>Click me</Button>;
+};
+
+export default SampleButton;
